207484963f9a59d9512ab6d0ac9ed8ff6c32fea0
vukovic98 <vladimirvukovic98@hotmail.rs>
2020-04-06 18:25:36 +0200

    initial spring

---
+++ team16/src/main/java/com/clinic/team16/Team16Application.java
@@ -0,0 +1,13 @@
+package com.clinic.team16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team16Application.class, args);
+	}
+
+}
---
+++ team16/src/test/java/com/clinic/team16/Team16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.team16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
