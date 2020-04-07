ebb61a78b9c20c209886ea3f179da827c76948a5
vukovic98 <vladimirvukovic98@hotmail.rs>
2020-04-07 18:44:23 +0200

    NurseService.java NurseRepository.java

--- team16/src/main/java/com/clinic/team16/repository/NurseRepository.java
+++ team16/src/main/java/com/clinic/team16/repository/NurseRepository.java
@@ -1,5 +1,19 @@
 package com.clinic.team16.repository;
 
-public class NurseRepository {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Repository;
+
+import com.clinic.team16.beans.Nurse;
+
+@Repository
+public class NurseRepository {
+	
+	private ArrayList<Nurse> nurses = new ArrayList<>();
+	
+	public Nurse create(Nurse nurse) {
+		this.nurses.add(nurse);
+		
+		return nurse;
+	}
 }
