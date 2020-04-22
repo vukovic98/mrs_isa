e5844e341f7d4a73ffa80c809be0e574e3a1d307
ducom98 <ducom98@gmail.com>
2020-04-22 20:46:13 +0200

    model changes

---
+++ team16/src/main/java/com/clinic/team16/beans/ClinicalCenterAdministrator.java
@@ -0,0 +1,101 @@
+/***********************************************************************
+ * Module:  ClinicalCenterAdministrator.java
+ * Author:  Vladimir
+ * Purpose: Defines the Class ClinicalCenterAdministrator
+ ***********************************************************************/
+
+import java.util.*;
+
+public class ClinicalCenterAdministrator extends User {
+   public ClinicalCenter clinicalCenter;
+   public ArrayList<RegistrationRequest> registrationRequest;
+   public AppointmentRequest[] appointmentRequest;
+   
+   
+   
+   
+   public ClinicalCenterAdministrator() {
+	super();
+}
+
+public ClinicalCenterAdministrator(ClinicalCenter clinicalCenter, ArrayList<RegistrationRequest> registrationRequest,
+		AppointmentRequest[] appointmentRequest) {
+	super();
+	this.clinicalCenter = clinicalCenter;
+	this.registrationRequest = registrationRequest;
+	this.appointmentRequest = appointmentRequest;
+}
+
+
+
+public ClinicalCenter getClinicalCenter() {
+	return clinicalCenter;
+}
+
+public void setClinicalCenter(ClinicalCenter clinicalCenter) {
+	this.clinicalCenter = clinicalCenter;
+}
+
+public AppointmentRequest[] getAppointmentRequest() {
+	return appointmentRequest;
+}
+
+public void setAppointmentRequest(AppointmentRequest[] appointmentRequest) {
+	this.appointmentRequest = appointmentRequest;
+}
+
+public ArrayList<RegistrationRequest> getRegistrationRequest() {
+      if (registrationRequest == null)
+         registrationRequest = new java.util.HashSet<RegistrationRequest>();
+      return registrationRequest;
+   }
+   
+   public java.util.Iterator getIteratorRegistrationRequest() {
+      if (registrationRequest == null)
+         registrationRequest = new ArrayList<RegistrationRequest>();
+      return registrationRequest.iterator();
+   }
+   
+   public void setRegistrationRequest(ArrayList<RegistrationRequest> newRegistrationRequest) {
+      removeAllRegistrationRequest();
+      for (java.util.Iterator iter = newRegistrationRequest.iterator(); iter.hasNext();)
+         addRegistrationRequest((RegistrationRequest)iter.next());
+   }
+   
+   public void addRegistrationRequest(RegistrationRequest newRegistrationRequest) {
+      if (newRegistrationRequest == null)
+         return;
+      if (this.registrationRequest == null)
+         this.registrationRequest = new ArrayList<RegistrationRequest>();
+      if (!this.registrationRequest.contains(newRegistrationRequest))
+      {
+         this.registrationRequest.add(newRegistrationRequest);
+         newRegistrationRequest.setClinicalCenterAdministrator(this);      
+      }
+   }
+   
+   public void removeRegistrationRequest(RegistrationRequest oldRegistrationRequest) {
+      if (oldRegistrationRequest == null)
+         return;
+      if (this.registrationRequest != null)
+         if (this.registrationRequest.contains(oldRegistrationRequest))
+         {
+            this.registrationRequest.remove(oldRegistrationRequest);
+            oldRegistrationRequest.setClinicalCenterAdministrator((ClinicalCenterAdministrator)null);
+         }
+   }
+   
+   public void removeAllRegistrationRequest() {
+      if (registrationRequest != null)
+      {
+         RegistrationRequest oldRegistrationRequest;
+         for (java.util.Iterator iter = getIteratorRegistrationRequest(); iter.hasNext();)
+         {
+            oldRegistrationRequest = (RegistrationRequest)iter.next();
+            iter.remove();
+            oldRegistrationRequest.setClinicalCenterAdministrator((ClinicalCenterAdministrator)null);
+         }
+      }
+   }
+
+}
---
+++ team16/src/main/java/com/clinic/team16/beans/MedicalRecord.java
@@ -0,0 +1,103 @@
+/***********************************************************************
+ * Module:  MedicalRecord.java
+ * Author:  Vladimir
+ * Purpose: Defines the Class MedicalRecord
+ ***********************************************************************/
+
+import java.util.*;
+
+public class MedicalRecord {
+   private int height;
+   private int weight;
+   private String bloodType;
+   private int bloodPressure;
+   private Collection<String> allergies;
+   private Collection<String> perscriptions;
+   private String medicalHistory;
+   
+   public Patient patient;
+
+public MedicalRecord() {
+	super();
+}
+
+public MedicalRecord(int height, int weight, String bloodType, int bloodPressure, Collection<String> allergies,
+		Collection<String> perscriptions, String medicalHistory, Patient patient) {
+	super();
+	this.height = height;
+	this.weight = weight;
+	this.bloodType = bloodType;
+	this.bloodPressure = bloodPressure;
+	this.allergies = allergies;
+	this.perscriptions = perscriptions;
+	this.medicalHistory = medicalHistory;
+	this.patient = patient;
+}
+
+public int getHeight() {
+	return height;
+}
+
+public void setHeight(int height) {
+	this.height = height;
+}
+
+public int getWeight() {
+	return weight;
+}
+
+public void setWeight(int weight) {
+	this.weight = weight;
+}
+
+public String getBloodType() {
+	return bloodType;
+}
+
+public void setBloodType(String bloodType) {
+	this.bloodType = bloodType;
+}
+
+public int getBloodPressure() {
+	return bloodPressure;
+}
+
+public void setBloodPressure(int bloodPressure) {
+	this.bloodPressure = bloodPressure;
+}
+
+public Collection<String> getAllergies() {
+	return allergies;
+}
+
+public void setAllergies(Collection<String> allergies) {
+	this.allergies = allergies;
+}
+
+public Collection<String> getPerscriptions() {
+	return perscriptions;
+}
+
+public void setPerscriptions(Collection<String> perscriptions) {
+	this.perscriptions = perscriptions;
+}
+
+public String getMedicalHistory() {
+	return medicalHistory;
+}
+
+public void setMedicalHistory(String medicalHistory) {
+	this.medicalHistory = medicalHistory;
+}
+
+public Patient getPatient() {
+	return patient;
+}
+
+public void setPatient(Patient patient) {
+	this.patient = patient;
+}
+
+   
+   
+}
