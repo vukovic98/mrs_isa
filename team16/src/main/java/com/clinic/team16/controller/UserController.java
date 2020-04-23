f7c484ebaebd226c4dd4c522a2f0a9e1b9ac596d
ducom98 <ducom98@gmail.com>
2020-04-23 17:30:51 +0200

    fixes

--- team16/src/main/java/com/clinic/team16/controller/UserController.java
+++ team16/src/main/java/com/clinic/team16/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.clinic.team16.service.AppointmentService;
 import com.clinic.team16.service.ClinicalCenterService;
+import com.clinic.team16.service.UserService;
 
 @RestController
 @RequestMapping("/userApi")
--- team16/src/main/java/com/clinic/team16/service/OrdinationService.java
+++ team16/src/main/java/com/clinic/team16/service/OrdinationService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.clinic.team16.repository.AppointmentRepository;
+import com.clinic.team16.repository.OrdinationRepository;
 
 @Service
 public class OrdinationService {
