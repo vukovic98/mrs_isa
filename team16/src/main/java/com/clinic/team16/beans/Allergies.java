f93b0718eb68c0a875fb92925bf3851ebb08445e
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2020-04-28 19:49:01 +0200

    Added allergies

--- team16/src/main/java/com/clinic/team16/beans/Allergies.java
+++ team16/src/main/java/com/clinic/team16/beans/Allergies.java
@@ -1,5 +1,23 @@
 package com.clinic.team16.beans;
 
 public enum Allergies {
-
+	
+	POLLEN,
+	GRASS,
+	DUST,
+	PENICILLIN,
+	EGGS,
+	FISH,
+	MILK,
+	PEANUTS,
+	NUTS,
+	SOY,
+	WHEAT,
+	LATEX,
+	METALS,
+	AMBROSION,
+	PET,
+	GLUTEN,
+	INSECT
+	
 }
