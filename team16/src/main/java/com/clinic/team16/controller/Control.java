76c36c380435c6371b7fc6172e952cf7d42814c1
vukovic98 <vladimirvukovic98@hotmail.rs>
2020-04-30 14:25:02 +0200

    bugs

--- team16/src/main/java/com/clinic/team16/controller/Control.java
+++ team16/src/main/java/com/clinic/team16/controller/Control.java
@@ -47,17 +47,24 @@ public class Control {
 	   public String medications() {
 		   return "medications";
 	   }
+	   
 	   @RequestMapping(value = "/medicalRecord")
 	   public String medicalRecord() {
 		   return "medicalRecord";
 	   }
+	   
 	   @RequestMapping(value = "/patientsClinics")
 	   public String patientsClinics() {
 		   return "patientsClinics";
 	   }
+	   
 	   @RequestMapping(value = "/appointmentHistory")
 	   public String appointmentHistory() {
 		   return "appointmentHistory";
 	   }
 	   
+	   @RequestMapping(value = "/diagnosis")
+	   public String diagnosis() {
+		   return "diagnosis";
+	   }
 }
