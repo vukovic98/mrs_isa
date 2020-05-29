4752cd085fe2da93cc727de38e881e34f14f3aaf
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2020-05-29 16:05:58 +0200

    bugs

--- team16/src/main/java/com/clinic/team16/service/ClinicService.java
+++ team16/src/main/java/com/clinic/team16/service/ClinicService.java
@@ -1,6 +1,7 @@
 package com.clinic.team16.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
