4cd22acc1ae11bab60bb6171ad76043ead18ac5f
dusanmadzarevic <ducom98@gmail.com>
2020-06-14 11:50:05 +0200

    US-32

--- team16/src/main/java/com/clinic/team16/controller/PricelistItemController.java
+++ team16/src/main/java/com/clinic/team16/controller/PricelistItemController.java
@@ -6,17 +6,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clinic.team16.beans.ClinicAdministrator;
 import com.clinic.team16.beans.Pricelist;
 import com.clinic.team16.beans.PricelistItem;
 import com.clinic.team16.beans.DTO.PricelistItemDTO;
+import com.clinic.team16.service.ClinicAdminService;
 import com.clinic.team16.service.PricelistItemService;
 import com.clinic.team16.service.PricelistService;
 
@@ -30,6 +34,8 @@ public class PricelistItemController {
 	@Autowired
 	private PricelistService pricelistService;
 
+	@Autowired
+	private ClinicAdminService adminService;
 
 	@GetMapping(path = "/findAll")
 	public ResponseEntity<List<PricelistItem>> findAll() {
@@ -62,14 +68,19 @@ public class PricelistItemController {
 	@PostMapping(path = "/addPricelistItem", consumes = "application/json")
 	public ResponseEntity<HttpStatus> addPricelistItem(@RequestBody PricelistItemDTO p) {
 		PricelistItem found = this.pricelistItemService.findOneByAppointmentType(p.getAppointmentType());
-		
+		String currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
+		ClinicAdministrator ca = adminService.findOneByEmail(currentUser);
 		if (found == null) {
 			PricelistItem pri = new PricelistItem(p.getAppointmentType(), Long.parseLong(p.getPrice()), null);
-			Pricelist prList = pricelistService.findOneByPricelistId(1);
+			Pricelist prList = pricelistService.findOneByPricelistId(ca.getClinic().getPricelist().getPricelistId());
 
 			pri.setPricelist(prList);
 			PricelistItem k = this.pricelistItemService.save(pri);
 
+			prList.getPricelistItems().add(k);
+			pricelistService.save(prList);
+			
+			
 			if (k != null)
 				return new ResponseEntity<>(HttpStatus.OK);
 			else
@@ -85,8 +96,28 @@ public class PricelistItemController {
 		PricelistItem pr = this.pricelistItemService.findOneByPricelistItemId(Long.parseLong(id));
 
 		if (pr != null) {
+			Pricelist prList = pr.getPricelist();
+			prList.getPricelistItems().remove(pr);
+			
+			
 			pr.setPricelist(null);
 			this.pricelistItemService.delete(pr);
+			this.pricelistService.save(prList);
+
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PutMapping(path = "/editPricelistItem", consumes = "application/json")
+	public ResponseEntity<HttpStatus> editPricelistItem(@RequestBody PricelistItemDTO p) {
+		System.out.println("id je " + p.getId());
+		PricelistItem pr = this.pricelistItemService.findOneByPricelistItemId(Long.parseLong(p.getId()));
+
+		if (pr != null) {
+			pr.setName(p.getAppointmentType());
+			pr.setPrice(Double.valueOf(p.getPrice()));
 
 			return new ResponseEntity<>(HttpStatus.OK);
 		} else {
